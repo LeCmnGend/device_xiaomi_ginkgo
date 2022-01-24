5d07eccbe7afdaee001c93ac7a257f64e2d68e3b
LeCmnGend <namhoang1235@gmail.com>
2022-01-24 11:31:59 +0700

    Build libbatterylistener from source

--- device.mk
+++ device.mk
@@ -88,7 +88,10 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
-    audio.usb.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    libbatterylistener \
     libaudio-resampler \
     libaudioroute \
     libtinycompress \
@@ -116,6 +119,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+
 
 # Bluetooth
 PRODUCT_PACKAGES += \
