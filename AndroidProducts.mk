6504ff6e715c23dbb9c82d17b55de547e1cb41cb
LeCmnGend <namhoang1235@gmail.com>
2022-09-15 01:07:14 +0700

    ginkgo: Bringup Superior OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/superior_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    superior_ginkgo-user \
+    superior_ginkgo-userdebug \
+    superior_ginkgo-eng
